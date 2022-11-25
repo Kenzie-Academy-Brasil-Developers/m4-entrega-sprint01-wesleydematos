fc44096e799100dcdd0a60082d362c79dea5d345
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-25 19:57:11 -0300

    feat: create POST and GET route /users

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 import express from "express";
+import users from "./database";
+import { v4 as uuidv4 } from "uuid";
 
+const app = express();
+app.use(express.json());
 
-const app = express()
+app.post("/users", (req, res) => {
+  let user = {
+    ...req.body,
+    createdOn: new Date(),
+    updatedOn: new Date(),
+    uuid: uuidv4(),
+  };
+  users.push(user);
 
+  let userToShow = user;
+  delete userToShow.password;
 
-export default app
+  return res.status(201).json(userToShow);
+});
+
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/users/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const foundUser = users.find((user) => user.uuid === userId);
+
+  if (!foundUser) {
+    return res.status(404).json({ message: "User not found!" });
+  }
+
+  return res.json(foundUser);
+});
+
+app.listen(3000, () => {
+  console.log("Server running in port 3000");
+});
+
+export default app;
