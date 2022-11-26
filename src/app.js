5da65c7b392f14e356917da23ef2795a4289a2d7
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-11-25 21:28:05 -0300

    fix: create services and crontrollers

--- src/app.js
+++ src/app.js
@@ -5,9 +5,10 @@ import { v4 as uuidv4 } from "uuid";
 const app = express();
 app.use(express.json());
 
-app.post("/users", (req, res) => {
+//services
+const createUserService = (userData) => {
   let user = {
-    ...req.body,
+    ...userData,
     createdOn: new Date(),
     updatedOn: new Date(),
     uuid: uuidv4(),
@@ -17,26 +18,38 @@ app.post("/users", (req, res) => {
   let userToShow = { ...user };
   delete userToShow.password;
 
-  return res.status(201).json(userToShow);
-});
+  return [201, userToShow];
+};
 
-app.get("/users", (req, res) => {
+const listUsersService = () => {};
+
+//controllers
+const createUserController = (req, res) => {
+  const [status, data] = createUserService(req.body);
+
+  return res.status(status).json(data);
+};
+
+const listUsersController = (req, res) => {
   return res.json(users);
-});
+};
 
-app.get("/users/:userId", (req, res) => {
-  const userId = req.params.userId;
-  const foundUser = users.find((user) => user.uuid === userId);
-
-  if (!foundUser) {
-    return res.status(404).json({ message: "User not found!" });
-  }
-
-  return res.json(foundUser);
-});
+app.post("/users", createUserController);
+app.get("/users", listUsersController);
 
 app.listen(3000, () => {
   console.log("Server running in port 3000");
 });
 
 export default app;
+
+// app.get("/users/:userId", (req, res) => {
+//   const userId = req.params.userId;
+//   const foundUser = users.find((user) => user.uuid === userId);
+
+//   if (!foundUser) {
+//     return res.status(404).json({ message: "User not found!" });
+//   }
+
+//   return res.json(foundUser);
+// });
